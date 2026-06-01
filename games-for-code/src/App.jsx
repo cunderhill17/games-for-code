468ad99579b5be53f8402ab1b256fb37c2ae5fe6
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-06-01 17:22:57 -0400

    feat: create basic routing
    Create basic routing to a 'homepage' with the ability to add further pages in the future

--- games-for-code/src/App.jsx
+++ games-for-code/src/App.jsx
@@ -1,11 +1,33 @@
-import './App.css'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+
+import Homepage from './Homepage'
+
+function AppLayout() {
+
+    return (
+        <>      
+            <Outlet/>       
+        </>
+    )
+}
+
+
+const router = createBrowserRouter([
+    { 
+        element: <AppLayout />,
+
+        children: [
+            { path: '/',          element: <Homepage /> }
+        ]
+    }
+])
+
+
 
 function App() {
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
