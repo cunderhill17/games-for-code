95bced2273d98a8fff82423c61bde632f6b14511
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-06-08 10:20:42 -0400

    refactor: integrate NavLink into Header and add styling IDs
    Reworked the Header navigation to use NavLink and introduced IDs for more precise styling control. Also imported Header styling file

--- games-for-code/src/Header.jsx
+++ games-for-code/src/Header.jsx
@@ -1,13 +1,18 @@
+import headerStyles from './styles/components/Header.module.scss';
+import { NavLink } from "react-router-dom";
+
 export default function Header() {
     return (
-        <header className="grid-con">
+        <header id={headerStyles['page-header']} className="grid-con">
             <div className="col-span-1 md:col-span-3 lg:col-span-3">
-                <p>Logo</p>
+                <NavLink to="/">
+                    <p>Logo</p>
+                </NavLink>
             </div>
-            <nav className="col-span-3 md:col-start-6 md:col-end-9 lg:col-start-10 lg:col-end-13">
-                <ul className="center-item">
-                    <li><a href="#">Games</a></li>
-                    <li><a href="#">About</a></li>
+            <nav id={headerStyles['main-nav']} className="col-span-3 md:col-start-6 md:col-end-9 lg:col-start-10 lg:col-end-13">
+                <ul>
+                    <li><NavLink to="/memory">Games</NavLink></li>
+                    <li><NavLink to="/">About</NavLink></li>
                 </ul>
             </nav>
         </header>
