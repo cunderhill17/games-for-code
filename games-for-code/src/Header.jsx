f3693b4a23e75a44b55b6a06ef1051d65571ce84
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-06-08 09:52:12 -0400

    feat: add HTML structure for header navigation
    Added the initial HTML element layout for the header navigation component.

--- games-for-code/src/Header.jsx
+++ games-for-code/src/Header.jsx
@@ -1,5 +1,15 @@
 export default function Header() {
     return (
-        <h1>This is my Header</h1>
+        <header className="grid-con">
+            <div className="col-span-1 md:col-span-3 lg:col-span-3">
+                <p>Logo</p>
+            </div>
+            <nav className="col-span-3 md:col-start-6 md:col-end-9 lg:col-start-10 lg:col-end-13">
+                <ul className="center-item">
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
