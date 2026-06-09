ad0c3f6df98b94efe79f59ed8a837b1d4f953215
cunderhill17 <93687857+cunderhill17@users.noreply.github.com>
2026-06-09 09:01:37 -0400

    feat: add HTML for Games component
    Added basic HTML structure for the Games component. Current card values are hard‑coded temporarily until the design phase for the component is complete.

--- games-for-code/src/Games.jsx
+++ games-for-code/src/Games.jsx
@@ -1,7 +1,46 @@
+import { NavLink } from "react-router-dom"
+
 export default function Games() {
+    // const gameInfo = [
+    //     {
+    //         name: "Code Match",
+    //         available: true,
+    //         image: 'images/code-match.jpg',
+    //         link: '/memory'
+    //     }
+    // ]
+
+
     return (
         <main>
-            <h2>This is the games page</h2>
+            <section className="grid-con">
+                <h2 className="col-span-full center-item">Games</h2>
+                <h3 className="col-span-full center-item">Browse and Level Up Your Skills!</h3>
+            </section>
+
+            <section className="grid-con">
+                <div className="games-con">
+                    <GameCard />
+                </div>
+            </section>
+
         </main>
+    )
+}
+
+
+function GameCard() {
+    return (
+        <NavLink to="/memory">
+            <section className="pos-relative">
+                <div>
+                    <h2>Code Match</h2>
+                    <img src="images/code-match.jpg" alt="Code Match Game Image" />
+                </div>
+                <div>
+                    <img src="images/play-button.svg" alt="Play Button Icon" />
+                </div>
+            </section>
+        </NavLink>
     )
 }
